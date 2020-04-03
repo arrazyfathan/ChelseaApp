5753bb9300b7e0480c4e5e46bfe4c9e80ee1143e
Ar Razy Fathan Rabbani <arrazy.rabbani266@gmail.com>
2020-04-03 13:20:25 +0700

    add splash screen

--- app/src/main/java/com/dicoding/chelseaapp/MainActivity.java
+++ app/src/main/java/com/dicoding/chelseaapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.dicoding.chelseaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean doubleBackToExit =false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (doubleBackToExit){
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExit = true;
+        Toast.makeText(this, "Press again to exit",Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExit=false;
+            }
+        }, 2000);
     }
 }
---
+++ app/src/main/java/com/dicoding/chelseaapp/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.dicoding.chelseaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.colorGray));
+
+        //Splash Screen
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(1200);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+}
